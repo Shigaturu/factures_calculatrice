82e12e9efb6b8fc5934f8f43c79d53f5ea02ec7f
Brian Fraisse <fraissebrian47@gmail.com>
2023-10-11 09:45:12 +0200

    Ajoutez des fichiers projet.

---
+++ Factures/Program.cs
@@ -0,0 +1,70 @@
+﻿const float _plafondTTC = 36800;
+float sommeFacture = 0;
+bool done2 = false;
+
+float nbFacture = getNumberFromUser();
+
+while (!done2)
+{
+    try
+    {
+        for (int i = 0; i < nbFacture; i++)
+        {
+            Console.WriteLine("Quel est le montant de la facture " + i);
+            float prixFacture = Convert.ToInt32(Console.ReadLine());
+            sommeFacture += prixFacture;
+
+        }
+        done2 = true;
+    }
+
+    catch
+    {
+        Console.WriteLine("Saisie incorect");
+
+    }
+}
+
+Console.WriteLine("Somme des factures en brut annuel " + sommeFacture + "€");
+Console.WriteLine("Somme des factures en net annuel " + getNet(sommeFacture) + "€");
+Console.WriteLine("Facture par mois net " + getMensuel(getNet(sommeFacture)) + "€");
+Console.WriteLine("Facture par mois brut " + getMensuel(sommeFacture) + "€");
+
+if (sommeFacture > _plafondTTC)
+{
+    Console.WriteLine("Le plafond brut est dépassé de " + (sommeFacture - _plafondTTC) + "€");
+}
+
+
+float getNet(float brut)
+{
+    return brut * (float)0.75;
+}
+
+float getMensuel(float annuel)
+{
+    return annuel / (float)12;
+}
+
+float getNumberFromUser()
+{
+    float nbFacture = 0;
+    bool done = false;
+
+    while (!done)
+    {
+        try
+        {
+            Console.WriteLine("Entrez le nombre de factures");
+            nbFacture = Convert.ToInt32(Console.ReadLine());
+            done = true;
+
+        }
+        catch
+        {
+            Console.WriteLine("Saisie incorect");
+
+        }
+    }
+    return nbFacture;
+}
---
+++ test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.RegularExpressions;
+
+bool done2 = false;
+while (!done2)
+{
+    try
+    {
+        Console.WriteLine("Saisir l'opération ");
+        string operation = Console.ReadLine();
+
+        MatchCollection operatorMatches = Regex.Matches(operation, @"[\+\-\*\/]");
+        string[] operators = new string[operatorMatches.Count];
+
+        MatchCollection matches = Regex.Matches(operation, @"\d+");
+        int[] numbers = new int[matches.Count];
+
+        for (int i = 0; i < matches.Count; i++)
+        {
+            numbers[i] = int.Parse(matches[i].Value);
+        }
+
+        for (int i = 0; i < operatorMatches.Count; i++)
+        {
+            operators[i] = operatorMatches[i].Value;
+        }
+
+        int[] numbersInt = numbers.ToArray();
+
+
+
+        int result = numbersInt[0];
+
+        for (int i = 0; i < operators.Length; i++)
+        {
+            int nextNumber = numbersInt[i + 1];
+            string currentOperator = operators[i];
+
+            switch (currentOperator)
+            {
+                case "+":
+                    result += nextNumber;
+                    break;
+                case "-":
+                    result -= nextNumber;
+                    break;
+                case "*":
+                    result *= nextNumber;
+                    break;
+                case "/":
+                    if (nextNumber != 0)
+                        result /= nextNumber;
+                    else
+                    {
+                        Console.WriteLine("Division par zéro n'est pas autorisée.");
+                        
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opérateur non pris en charge.");
+                     
+                    break;
+            }
+        }
+
+        Console.WriteLine("Le résultat de l'opération est : " + result);
+        done2 = true;
+        
+
+    }
+    catch
+    {
+        Console.WriteLine("Saisie incorrecte");
+    }
+}
